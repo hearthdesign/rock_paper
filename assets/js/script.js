aeae27e93e1625303251b3e796c02bea0d2b85c7
hearthdesign <hearthxdesign@gmail.com>
2025-07-04 16:58:36 +0200

    add player and computer selection display to game interface

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("connected");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const winningMove = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"]
+};
+
+
