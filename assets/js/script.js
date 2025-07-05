dffda5b644d17841ef0990ae60d54f086e6263e8
hearthdesign <hearthxdesign@gmail.com>
2025-07-05 11:04:16 +0200

    declare variables and constants and element selections forfunctionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,19 @@ const winningMove = {
     spock: ["scissors", "rock"]
 };
 
+let playerScore= 0;
+let computerScore= 0;
+
+const reultElement = document.getElementById("result");
+const playerScoreElement = document.getElementById("playerScore");
+const computerScoreElement = document.getElementById("computerScore");
+
+const playerChoiceImg = document.getElementById("playerChoice");
+const computerChoiceImg = document.getElementById("computerChoice");
+
+const resetButton = document.getElementById("reset-btn");
+
+const moveButtons = document.querySelectorAll(".move-button");
+
+
+
